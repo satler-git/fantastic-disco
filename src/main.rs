90970bc6397c438429fc54be46249cbe54b94122
satler <119724672+satler-git@users.noreply.github.com>
2024-06-12 00:28:15 +0000

    Add embassy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use embassy_executor::Spawner;
+use embassy_futures::select::{select, Either};
+use embassy_time::Duration;
+use microbit_bsp::*;
+use {defmt_rtt as _, panic_probe as _};
 
-#[entry]
-fn main() -> ! {
-    loop {}
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let board = Microbit::default();
+
+    let mut display = board.display;
+    let mut btn_a = board.btn_a;
+    let mut btn_b = board.btn_b;
+
+    display.set_brightness(display::Brightness::MAX);
+    display.scroll("Hello, World!").await;
+    defmt::info!("Application started, press buttons!");
+    loop {
+        match select(btn_a.wait_for_low(), btn_b.wait_for_low()).await {
+            Either::First(_) => {
+                defmt::info!("A pressed");
+                display
+                    .display(display::fonts::ARROW_LEFT, Duration::from_secs(1))
+                    .await;
+            }
+            Either::Second(_) => {
+                defmt::info!("B pressed");
+                display
+                    .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
+                    .await;
+            }
+        }
+    }
 }
