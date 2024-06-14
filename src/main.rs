daee4cb32ac9a22bb887b5f5e3432174cd5e637e
satler <119724672+satler-git@users.noreply.github.com>
2024-06-14 12:29:56 +0000

    Press A and B to print hello world

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_futures::select::{select, Either};
+use embassy_nrf::gpio::Level;
 use embassy_time::Duration;
 use microbit_bsp::*;
 use {defmt_rtt as _, panic_probe as _};
@@ -12,27 +12,34 @@ async fn main(_spawner: Spawner) {
     let board = Microbit::default();
 
     let mut display = board.display;
-    let mut btn_a = board.btn_a;
-    let mut btn_b = board.btn_b;
+    let btn_a = board.btn_a;
+    let btn_b = board.btn_b;
 
     display.set_brightness(display::Brightness::MAX);
     display.scroll("Hello, World!").await;
     defmt::info!("Application started, press buttons!");
     loop {
-        match select(btn_a.wait_for_low(), btn_b.wait_for_low()).await {
-            Either::First(_) => {
+        match (btn_a.get_level(), btn_b.get_level()) {
+            (Level::Low, Level:: High) => {
                 defmt::info!("A pressed");
                 display
                     .display(display::fonts::ARROW_LEFT, Duration::from_secs(1))
                     .await;
             }
-            Either::Second(_) => {
+            (Level::High, Level::Low) => {
                 defmt::info!("B pressed");
                 display
                     .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
                     .await;
             }
+            (Level::Low, Level::Low) => {
+                defmt::info!("A and B pressed");
+                display
+                    .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
+                    .await;
+                display.scroll("Hello, World!").await;
+            }
+            _ => {}
         }
-        display.scroll("Hello, World!").await;
     }
 }
