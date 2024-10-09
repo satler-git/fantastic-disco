cd9f0533d57b8d501e11033c0963badb3f762aab
satler <119724672+satler-git@users.noreply.github.com>
2024-10-09 03:11:06 +0000

    update some files

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rustup target add thumbv7em-none-eabihf
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
