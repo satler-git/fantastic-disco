f73372b16b80e3d0b44f3dfc17b27b0647f41647
satler <119724672+satler-git@users.noreply.github.com>
2024-10-09 03:12:48 +0000

    Add nix

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rustup target add thumbv7em-none-eabihf
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
