5c2896f3368a5b1211cc28053dced7a22fa8ad1b
satler <119724672+satler-git@users.noreply.github.com>
2024-10-16 02:04:35 +0000

    Play sound

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use embassy_nrf::gpio::Level;
 use embassy_time::{Duration, Timer};
 use microbit_bsp::*;
 use {defmt_rtt as _, panic_probe as _};
+use  microbit_bsp::speaker::Note;
+use microbit_bsp::speaker::Pitch;
+use microbit_bsp::speaker::NamedPitch;
+use microbit_bsp::speaker::PwmSpeaker;
+use embassy_nrf::pwm::SimplePwm;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -18,6 +23,8 @@ async fn main(_spawner: Spawner) {
     display.set_brightness(display::Brightness::MAX);
     display.scroll("Hello, World!").await;
     defmt::info!("Application started, press buttons!");
+    let sound = Note(Pitch::Named(NamedPitch::A4), 1000);
+    let mut speacker = PwmSpeaker::new(SimplePwm::new_1ch(board.pwm0, board.speaker));
     loop {
         match (btn_a.get_level(), btn_b.get_level()) {
             (Level::Low, Level:: High) => {
@@ -28,9 +35,11 @@ async fn main(_spawner: Spawner) {
             }
             (Level::High, Level::Low) => {
                 defmt::info!("B pressed");
+                let playing = speacker.play(&sound);
                 display
                     .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
                     .await;
+                playing.await;
             }
             (Level::Low, Level::Low) => {
                 defmt::info!("A and B pressed");
