1af14fd3467cca7b12e8d36d801cec0bcb8451aa
satler <119724672+satler-git@users.noreply.github.com>
2024-10-16 02:27:59 +0000

    add tempreture measure

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ use microbit_bsp::*;
 use {defmt_rtt as _, panic_probe as _};
 use microbit_bsp::speaker::PwmSpeaker;
 use embassy_nrf::pwm::SimplePwm;
+use microbit::board::Board;
+use microbit::hal::temp::Temp;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let board = Microbit::default();
+    let board_not_embassy = Board::take().unwrap();
 
     let mut display = board.display;
     let btn_a = board.btn_a;
@@ -20,8 +23,18 @@ async fn main(_spawner: Spawner) {
     display.set_brightness(display::Brightness::new(128));
     defmt::info!("Application started, press buttons!");
     // let mut speacker = PwmSpeaker::new(SimplePwm::new_1ch(board.pwm0, board.speaker));
+    let mut tmp_sen = Temp::new(board_not_embassy.TEMP);
     loop {
         match (btn_a.get_level(), btn_b.get_level()) {
+            (Level::High, Level::Low) | (Level::Low, Level::High) => {
+                let temp: f32 = tmp_sen.measure().to_num();
+                let mut buf = [0u8; 10];
+                let s = format_no_std::show(
+                    &mut buf,
+                    format_args!("{} C", temp as i32),
+                ).unwrap();
+                display.scroll(&s).await;
+            }
             _ => (),
         }
         Timer::after_millis(100).await;
